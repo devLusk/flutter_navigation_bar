94df93696a5e1b426deb5116d8d5387ce66b77f4
Lucas Silva <lucasvida2005@gmail.com>
2025-03-25 18:41:59 -0300

    starting home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -5,6 +5,36 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(title: Text("Home"), centerTitle: true),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Column(
+              children: [
+                Text("Welcome to Home Page", style: TextStyle(fontSize: 20)),
+                Text("That's a simple example of BottomNavigationBar"),
+              ],
+            ),
+            SizedBox(height: 20),
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20)
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Image.asset('lib/images/image_01.jpg', ), // TODO: Descobrir como arredondar as todos
+                  Image.asset('lib/images/image_02.jpg'),
+                  Image.asset('lib/images/image_03.jpg'),
+                ],
+              )
+            )
+          ],
+        ),
+      ),
+    );
   }
 }

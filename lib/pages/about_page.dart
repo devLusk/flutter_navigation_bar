c33513038cf3fa6919c11ceb450f56b821f0c862
Lucas Silva <lucasvida2005@gmail.com>
2025-03-26 11:45:30 -0300

    adding margin

--- lib/pages/about_page.dart
+++ lib/pages/about_page.dart
@@ -8,38 +8,41 @@ class AboutPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.white,
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text(
-              "ABOUT ME",
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 10),
-            Text(
-              "I'm currently learning about Flutter and exploring its capabilities. "
-              "This page is another example of how to use Flutter to create "
-              "simple, yet functional user interfaces. My goal is to build "
-              "beautiful and efficient mobile applications while improving "
-              "my skills in Dart and Flutter development.",
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 16, height: 1.5),
-            ),
-            SizedBox(height: 30),
-            ElevatedButton(
-              onPressed: () {
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(
-                    content: Text(
-                      "My next project will be a task management app!",
+        child: Container(
+          margin: EdgeInsets.all(32),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                "ABOUT ME",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 10),
+              Text(
+                "I'm currently learning about Flutter and exploring its capabilities. "
+                "This page is another example of how to use Flutter to create "
+                "simple, yet functional user interfaces. My goal is to build "
+                "beautiful and efficient mobile applications while improving "
+                "my skills in Dart and Flutter development.",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 16, height: 1.5),
+              ),
+              SizedBox(height: 30),
+              ElevatedButton(
+                onPressed: () {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text(
+                        "My next project will be a task management app!",
+                      ),
                     ),
-                  ),
-                );
-              },
-              child: Text("CLICK ME"),
-            ),
-          ],
+                  );
+                },
+                child: Text("CLICK ME"),
+              ),
+            ],
+          ),
         ),
       ),
     );
